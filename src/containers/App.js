4cc88a16a5446d6e2fa73f6d0cfc34eac19da864
HugoFranc <hugo_afranco@hotmail.com>
2023-02-13 03:35:30 -0600

    typo fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
-import ErrorBoundry from "../components/ErrorBoundry";
+import ErrorBoundary from "../components/ErrorBoundary";
 import './App.css'
 
 
@@ -40,9 +40,9 @@ class App extends Component {
                 <h1 className="f1"> RoboFriends </h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
-                    <ErrorBoundry>
+                    <ErrorBoundary>
                         <CardList robots={filteredRobots} />
-                    </ErrorBoundry>
+                    </ErrorBoundary>
                 </Scroll>
 
             </div>
